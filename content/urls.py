626386fd85514e1c5e470c9c4f2cdacb16962d61
King YouTube <167699038+Atabek0000@users.noreply.github.com>
2024-06-14 19:47:53 +0300

    Update urls.py

--- content/urls.py
+++ content/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('main/', views.main, name='home'),
+    path('', views.main, name='home'),
     path('o_nas/', views.o_nas, name='o_nas'),
     path('serif/', views.serif, name='serif'),
 ]
