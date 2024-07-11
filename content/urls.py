cc5112f4a0be7bc7a1ba09a272aac266ed58f6ce
King YouTube <167699038+Atabek0000@users.noreply.github.com>
2024-07-11 13:02:52 +0300

    Delete content directory

--- content/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.main, name='home'),
-    path('o_nas/', views.o_nas, name='o_nas'),
-    path('register/', views.register, name='register'),
-]
