9bdc60684c60770772dd7544239322a3a1a23967
Atabek200 <akylbekovatabek564@gmail.com>
2024-07-15 16:51:17 +0600

    1

--- itproger/settings.py
+++ itproger/settings.py
@@ -4,7 +4,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -14,7 +13,19 @@ SECRET_KEY = 'django-insecure-m-*-$m0z+v=!f0l&6z&*q!xb87rj@9=4wxs9*z7+d9h*qxn-cl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost', '127.0.0.1',
+    'techno.sench.student-project-it-academy.tw1.ru',
+    'www.techno.sench.student-project-it-academy.tw1.ru',
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://techno.sench.student-project-it-academy.tw1.ru',
+    'https://www.techno.sench.student-project-it-academy.tw1.ru',
+]
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 
 # Application definition
 
@@ -61,7 +72,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'itproger.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -71,7 +81,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -91,7 +100,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -102,7 +110,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
