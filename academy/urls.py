5fee79e4fc67fa2f5a85e6961882943c1cd89b94
King YouTube <167699038+Atabek0000@users.noreply.github.com>
2024-07-16 15:34:51 +0600

    Add files via upload

---
+++ academy/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('request/create/', views.create_request, name='create_request'),
+    path('client/', views.client_page, name='client_page'),
+    path('master/', views.master_page, name='master_page'),
+
+    path('accept/<int:request_id>/', views.accept_request, name='accept_request'),
+    path('reject/<int:request_id>/', views.reject_request, name='reject_request'),
+
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+]
